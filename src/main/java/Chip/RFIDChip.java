68a9f41f58d4b10dd6443c50fb28710543591312
marku <markus.hub95@googlemail.com>
2022-03-11 22:58:57 +0100

    Strategy-Pattern fertig!! jetzt HEFEEEEEEE

--- src/main/java/Chip/RFIDChip.java
+++ src/main/java/Chip/RFIDChip.java
@@ -2,10 +2,6 @@ package Chip;
 
 import Config.Configuration;
 
-import javax.crypto.*;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 //Da keiner aus unserem Team im ersten Teil die Komplexaufgabe mit dem RFID-Chip hatte, sind nicht alle Funktionen vorhanden.
 //Es wurde lediglich das Strategy-Pattern mit den verschiedenen Algorithmen angewandt.
 
